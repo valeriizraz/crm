5ef08a46dba01188191a2c5b217d79e5602c0145
valeriizraz <valeriizraz@gmail.com>
2023-05-25 15:25:48 +0200

    init project

---
+++ js/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// phone AppleX 
+const phoneAppleX = 'Apple X';
+const phoneAppleXQuantity = 54;
+const cellPhones = 'Мобильные телефоны';
+const phoneAppleXPrice = 999;
+
+console.log('telephone_apple_x'); 
